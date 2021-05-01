268105e096ebc2d68e75cac648475945ec0812dc
thxwelchs <thxwelchs@gmail.com>
2021-05-02 00:09:17 +0900

    refactor: code 정리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,5 @@
 import controller.RacingCarGame;
 import domain.CarNamesTokenizer;
-import java.util.List;
-import view.InputView;
 
 public class Application {
   public static void main(String[] args) {
